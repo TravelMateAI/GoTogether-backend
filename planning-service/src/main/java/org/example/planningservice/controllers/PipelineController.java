03044048e6a91f9428e53184ee488de99ed61548
Kavindu Senarathna <senarathnalpsuk.21@uom.lk>
2025-05-30 16:31:27 +0530

    auto generated changes of .idea package

--- planning-service/src/main/java/org/example/planningservice/controllers/PipelineController.java
+++ planning-service/src/main/java/org/example/planningservice/controllers/PipelineController.java
@@ -2,6 +2,8 @@ package org.example.planningservice.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.planningservice.pipelines.PipelineManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pipeline")
 public class PipelineController {
 
+    private static final Logger log = LoggerFactory.getLogger(PipelineController.class);
     private final PipelineManager<String> pipelineManager;
 
     public PipelineController(PipelineManager<String> pipelineManager) {
