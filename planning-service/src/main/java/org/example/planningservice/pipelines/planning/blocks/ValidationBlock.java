cc1262dcd484b37f246d239de712aab5bd5adce2
Kavindu Senarathna <senarathnalpsuk.21@uom.lk>
2025-06-12 10:35:39 +0530

    Merge branch 'dev' into dev_planning_kavindu_initiatePipelines

--- planning-service/src/main/java/org/example/planningservice/pipelines/planning/blocks/ValidationBlock.java
+++ planning-service/src/main/java/org/example/planningservice/pipelines/planning/blocks/ValidationBlock.java
@@ -1,14 +1,12 @@
-package org.example.planningservice.pipelines.steps;
+package org.example.planningservice.pipelines.planning.blocks;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.planningservice.pipelines.PipelineStep;
-import org.springframework.core.annotation.Order;
+import org.example.planningservice.frameworks.pipeline.Block;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@Order(1)
-public class ValidationStep implements PipelineStep<String> {
+public class ValidationBlock implements Block<String, String> {
 
     @Override
     public String process(String input) {
