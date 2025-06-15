836d0aaccb6b0bde3992160e06d064b6ee2ce85b
Harshana-2000 <lakshara.21@cse.mrt.ac.lk>
2025-06-15 21:42:35 +0530

    update

--- social-media-service/entrypoint.sh
+++ social-media-service/entrypoint.sh
@@ -4,9 +4,9 @@ until curl -sSf http://auth-service:8080/realms/kong/.well-known/openid-configur
   echo "Waiting for realm kong...";
   sleep 10;
 done
-# until curl -sSf -k https://gotogetheruom.duckdns.org:8443/realms/kong/.well-known/openid-configuration; do
-#   echo "Waiting for realm kong...";
-#   sleep 10;
-# done
+until curl -sSf -k https://gotogetheruom.duckdns.org:8446/realms/kong/.well-known/openid-configuration; do
+  echo "Waiting for realm kong...";
+  sleep 10;
+done
 echo "Keycloak realm is ready. Starting app..."
 exec java -jar /app.jar
